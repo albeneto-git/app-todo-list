6adde3d8b354ca2228a54038151f6c2c7181aaff
Albertino Neto <albeneto72@gmail.com>
2026-02-12 21:43:45 -0300

    Iniciando projeto. Estrutura do projeto, criação dos componentes e css.

---
+++ src/components/Task.jsx
@@ -0,0 +1,9 @@
+
+const Task = () => {
+    return <li>
+        <span>Texto da tarefa</span>
+        <button>Remover</button>
+    </li>
+};
+
+export default Task;
---
+++ src/components/TaskInput.jsx
@@ -0,0 +1,9 @@
+
+const TaskInput = () => {
+    return <form>
+        <input type="text" placeholder="Adicione uma nova tarefa"/>
+        <button type="submit">Adicionar</button>
+    </form>
+};
+
+export default TaskInput;

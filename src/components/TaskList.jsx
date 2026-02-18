61707b5b7153aa69d5b925b36470e7dedb1bdcfa
albeneto-git <albeneto72@gmail.com>
2026-02-18 15:24:40 -0300

    Adicionando onToggleTaskDone prop to TaskList component

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,13 +1,14 @@
 import Task from "./Task"
 
-const TaskList = ({tasks, onDeleteTask}) => {
+const TaskList = ({tasks, onDeleteTask, onToggleTaskDone}) => {
     return (
         <ul>
             {tasks.map((task) => (
                 <Task 
                     key={task.id} 
                     task={task} 
-                    onDelete={() => onDeleteTask(task.id)} />
+                    onDelete={() => onDeleteTask(task.id)} 
+                    onToggleDone={() => onToggleTaskDone(task.id)} />
             ))}
         </ul>
     )
